9cb367c8ba546e149eda46f62d46c51db1e0c9e0
Rafael Plugge <tini.shadow@email.com>
2018-09-27 17:57:43 +0100

    Started ai arrival system.

---
+++ AiLab/src/system/AiArriveSystem.cpp
@@ -0,0 +1,11 @@
+﻿#include "stdafx.h"
+#include "AiArriveSystem.h"
+
+app::sys::AiArriveSystem::AiArriveSystem(app::Registry & registry)
+	: BaseSystem(registry)
+{
+}
+
+void app::sys::AiArriveSystem::update(app::seconds const & dt)
+{
+}
---
+++ AiLab/src/system/AiArriveSystem.h
@@ -0,0 +1,39 @@
+﻿#ifndef _AI_ARRIVE_SYSTEM_H
+#define _AI_ARRIVE_SYSTEM_H
+
+#include "BaseSystem.h"
+
+namespace app::sys
+{
+	class AiArriveSystem : public BaseSystem
+	{
+	public: // Constructors/Destructor/Assignments
+		AiArriveSystem(app::Registry & registry);
+
+		AiArriveSystem() = delete;
+		AiArriveSystem(AiArriveSystem const &) = default;
+		AiArriveSystem(AiArriveSystem &&) = default;
+
+		~AiArriveSystem() = default;
+
+		AiArriveSystem & operator=(AiArriveSystem const &) = default;
+		AiArriveSystem & operator=(AiArriveSystem &&) = default;
+
+	public: // Public Static Functions
+	public: // Public Member Functions
+		virtual void update(app::seconds const & dt) override;
+	public: // Public Static Variables
+	public: // Public Member Variables
+	protected: // Protected Static Functions
+	protected: // Protected Member Functions
+	protected: // Protected Static Variables
+	protected: // Protected Member Variables
+	private: // Private Static Functions
+	private: // Private Member Functions
+	private: // Private Static Variables
+	private: // Private Member Variables
+
+	};
+}
+
+#endif // !_AI_ARRIVE_SYSTEM_H
